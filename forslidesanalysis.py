d3b57141a6cd7afe48fe8587d65d061fea3435fc
J-WAKE <M286744@USNA.EDU>
2026-04-20 17:34:05 -0400

    J-Wake added a python visualization code

--- forslidesanalysis.py
+++ forslidesanalysis.py
@@ -2,11 +2,14 @@ import pandas as pd
 import plotly.express as px
 
 bigone = pd.read_csv('./data/clean/finaltables.csv')
+percs = pd.read_csv('./data/clean/participation.csv')
+biggerone = pd.merge(bigone,percs, left_on=['recipient_state_name','action_date_fiscal_year'],right_on=['STATE','YEAR'], how='inner')
+biggerone = biggerone.drop(columns=['YEAR'])
 #print(bigone)
 
-ogsheet2023 = bigone[bigone['action_date_fiscal_year'] == 2023]
-ogsheet2024 = bigone[bigone['action_date_fiscal_year'] == 2024]
-ogsheet2025 = bigone[bigone['action_date_fiscal_year'] == 2025]
+ogsheet2023 = biggerone[biggerone['action_date_fiscal_year'] == 2023]
+ogsheet2024 = biggerone[biggerone['action_date_fiscal_year'] == 2024]
+ogsheet2025 = biggerone[biggerone['action_date_fiscal_year'] == 2025]
 
 state_to_code = {
     'ALABAMA': 'AL', 'ALASKA': 'AK', 'ARIZONA': 'AZ', 'ARKANSAS': 'AR', 'CALIFORNIA': 'CA',
@@ -22,23 +25,38 @@ state_to_code = {
     'VIRGINIA': 'VA', 'WASHINGTON': 'WA', 'WEST VIRGINIA': 'WV', 'WISCONSIN': 'WI', 'WYOMING': 'WY'
 }
 
-def data(sheet):
-    sheet = sheet.dropna()
-    sheet['state_code'] = sheet['recipient_state_name'].map(state_to_code)
-    sheet_act = sheet.sort_values(by='ACT_Score', ascending=False)
-    sheet_sat = sheet.sort_values(by='SAT_Score', ascending=False)
-    return sheet_act,sheet_sat
 
-actdata2023,satdata2023 = data(ogsheet2023)
-actdata2024,satdata2024 = data(ogsheet2024)
-actdata2025,satdata2025 = data(ogsheet2025)
+flor = [ogsheet2023,ogsheet2024,ogsheet2025]
+def fixit(f):
+    f = f.dropna()
+    f = f.drop(columns=['recipient_state_name'])
+    f['state_code'] = f['STATE'].map(state_to_code)
+    f['effectivness_act$1000'] = ((f['ACT_Score'] * (f['PARTICIPATION PERCENTAGE FOR ACT'] / 100 )) / f['money_per_student_per_state']) * 1000
+    f['effectivness_sat$1000'] = ((f['SAT_Score'] * (f['PARTICIPATION AVERAGE FOR SAT'] / 100 )) / f['money_per_student_per_state'])* 1000
+    return f
+
+newsheet2023=fixit(ogsheet2023)
+newsheet2024 = fixit(ogsheet2024)
+newsheet2025 = fixit(ogsheet2025)
+
+flor = [newsheet2023,newsheet2024,newsheet2025]
+for b in flor:
+    sheet_act = b.sort_values(by='effectivness_act$1000', ascending=False)
+    sheet_sat = b.sort_values(by='effectivness_sat$1000', ascending=False)
+    b['rank_act'] = b['effectivness_act$1000'].rank(ascending=False)
+    b['rank_sat'] = b['effectivness_sat$1000'].rank(ascending=False)
+    b['rank_diff'] = abs(b['rank_act'] - b['rank_sat'])
+    b['master_rank'] = (b['rank_act'] + b['rank_sat']) / 2
+    g = b[['STATE','action_date_fiscal_year','rank_act','rank_sat','rank_diff','master_rank']]
+    best_overall = g.sort_values('master_rank', ascending=False)
+    print(best_overall[['STATE','action_date_fiscal_year', 'master_rank', 'rank_act', 'rank_sat']].head(10))
 
 
 
-fig_SAT = px.scatter(actdata2023, 
+fig_SAT = px.scatter(newsheet2023, 
                  x='money_per_student_per_state', 
                  y='total_obligated_amount',  # Use the exact column name for your SAT scores
-                 text='recipient_state_name',    # This labels the'pi dots with state names
+                 text='STATE',    # This labels the'pi dots with state names
                  title='Dept. of Edu. Funds vs. money Per Student 2023',
                  
                  trendline="ols",
@@ -46,7 +64,7 @@ fig_SAT = px.scatter(actdata2023,
 
 graph = fig_SAT.write_html('state_scores.html')
 
-fig_fedspend = px.choropleth(satdata2025,
+fig_fedspend = px.choropleth(newsheet2023,
                     locations='state_code', 
                     locationmode="USA-states",
                     color='total_obligated_amount',
